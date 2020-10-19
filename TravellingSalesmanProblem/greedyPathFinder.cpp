dddfe972b9557dc6bfa8ee6c079fca45287463a3
juliabiel25 <67067000+juliabiel25@users.noreply.github.com>
2020-10-19 15:58:40 +0200

    Update greedyPathFinder.cpp
    Initial implementation of the greedy tsp algorithm

--- TravellingSalesmanProblem/greedyPathFinder.cpp
+++ TravellingSalesmanProblem/greedyPathFinder.cpp
@@ -1,8 +1,29 @@
 #include"World.h"
+#include <iostream>
+#include <limits>
 
 void World::greedyPathFinder()
 {
 
-
-
+	int start = 0, head = start;
+	for (int i = 0; i < this->numberOfPoints; i++) 
+	{
+		int minID = start;
+		double min = DBL_MAX;
+		std::cout << start << "\t";
+		for (int j = 0; j < this->numberOfPoints; j++) 
+		{
+			if (this->pathWeights[start][j] > -1 && this->pathWeights[start][j] < min) 
+			{
+				minID = j;
+				min = this->pathWeights[start][j];
+			}
+		}
+		for (int j = 0; j < this->numberOfPoints; j++) 
+		{
+			this->pathWeights[j][start] = -1;
+		}
+		start = minID;
+	}
+	std::cout << head << std::endl;
 }
