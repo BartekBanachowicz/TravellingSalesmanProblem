58a822cfff766b1dc9e0993adc8e7dda435b21ea
juliabiel25 <67067000+juliabiel25@users.noreply.github.com>
2020-11-17 14:39:38 +0100

    Upgraded the greedy algorithm

--- TravellingSalesmanProblem/greedyPathFinder.cpp
+++ TravellingSalesmanProblem/greedyPathFinder.cpp
@@ -4,36 +4,49 @@
 
 void World::greedyPathFinder()
 {
-	std::cout << "Greedy path found:\n";
+	double shortestTotal = DBL_MAX;
+	for (int h = 0; h < this->numberOfPoints; h++)
+	{
+		std::cout << "\nStarting with point no " << h + 1 << std::endl;
+		std::cout << "Greedy path found:\n";
 
-	int start = 0, head = start;
-	int minID = start;
-	double min = DBL_MAX, distance, distanceTotal = 0;
+		int start = h, head = start;
+		int minID = start;
+		double min = DBL_MAX, distance, distanceTotal = 0;
 
-	for (int i = 0; i < this->numberOfPoints; i++) 
-	{	
-		this->pointsMatrix[start].visit();		
-		distance = 0;
-		std::cout << start + 1 <<"\t";
-
-		for (int j = 0; j < this->numberOfPoints; j++) 
+		for (int i = 0; i < this->numberOfPoints; i++)
 		{
-			if (!this->pointsMatrix[j].ifVisited()) 
+			this->pointsMatrix[start].visit();
+			distance = 0;
+			std::cout << start + 1 << "\t";
+
+			for (int j = 0; j < this->numberOfPoints; j++)
 			{
-				distance = this->pointsMatrix[start].p2pDistance(this->pointsMatrix[j]);
-				if (distance < min) {
-					minID = j;
-					min = distance;
+				if (!this->pointsMatrix[j].ifVisited())
+				{
+					distance = this->pointsMatrix[start].p2pDistance(this->pointsMatrix[j]);
+					if (distance < min) {
+						minID = j;
+						min = distance;
+					}
 				}
 			}
+			this->pointsMatrix[minID].visit();
+			distanceTotal += distance;
+			start = minID;
+			min = DBL_MAX;
 		}
-		this->pointsMatrix[minID].visit();
-		distanceTotal += distance;
-		start = minID;
-		min = DBL_MAX;
-	}
-	std::cout << head + 1 << std::endl;
-	distanceTotal += this->pointsMatrix[minID].p2pDistance(this->pointsMatrix[head]);
-	std::cout << "Distance total: " << distanceTotal << std::endl;
+		std::cout << head + 1 << std::endl;
+		distanceTotal += this->pointsMatrix[minID].p2pDistance(this->pointsMatrix[head]);
+		std::cout << "Distance total: " << distanceTotal << std::endl << std::endl;
+		if (distanceTotal < shortestTotal)
+			shortestTotal = distanceTotal;
 
+		// cleanup
+		for (int i = 0; i < this->numberOfPoints; i++)
+			this->pointsMatrix[i].resetVisited();
+
+	}
+	std::cout << "Length of the shortest greedy path found: " << shortestTotal << std::endl << std::endl;
 }
+
